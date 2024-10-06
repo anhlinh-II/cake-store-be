92c7fb7df882e91c1d70b2f4baaf56833d00cd9c
Anh Linh <linhnguyenanh75@gmail.com>
2024-10-07 00:30:58 +0700

    getAll update delete product APIs

--- src/main/java/com/vn/cake_store/controller/ProductController.java
+++ src/main/java/com/vn/cake_store/controller/ProductController.java
@@ -1,14 +1,17 @@
 package com.vn.cake_store.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.vn.cake_store.dto.response.ApiResponse;
 import com.vn.cake_store.entity.Product;
 import com.vn.cake_store.service.ProductService;
+import java.util.List;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,7 +26,23 @@ public class ProductController {
           return this.productService.createProduct(product);
      }
 
+     @GetMapping("/{id}")
      public ApiResponse<Product> getProductById(@PathVariable Long id) {
           return this.productService.getProductById(id);
+     }
+
+     @GetMapping
+     public ApiResponse<List<Product>> getAllProduct() {
+          return this.productService.getAllProduct();
+     }
+
+     @PutMapping("/{id}")
+     public ApiResponse<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+          return this.productService.updateProduct(id, product);
+     }
+
+     @DeleteMapping("/{id}")
+     public ApiResponse<Void> deleteProduct(@PathVariable Long id) {
+          return this.productService.deleteProductById(id);
      }
 }
--- src/main/java/com/vn/cake_store/service/ProductService.java
+++ src/main/java/com/vn/cake_store/service/ProductService.java
@@ -3,6 +3,7 @@ package com.vn.cake_store.service;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.List;
 
 import com.vn.cake_store.dto.response.ApiResponse;
 import com.vn.cake_store.entity.Product;
@@ -39,11 +40,52 @@ public class ProductService {
           if (optionalProduct.isEmpty()) {
                throw new AppException(ErrorCode.PRODUCT_NOT_EXISTED);
           }
-          
+
           return ApiResponse.<Product>builder()
-          .code(1000)
-          .message("Get product with id " + id + " successfully!")
-          .result(optionalProduct.get())
-          .build();
+                    .code(1000)
+                    .message("Get product with id " + id + " successfully!")
+                    .result(optionalProduct.get())
+                    .build();
+     }
+
+     public ApiResponse<List<Product>> getAllProduct() {
+          return ApiResponse.<List<Product>>builder()
+                    .code(1000)
+                    .message("Get all product succesfully!")
+                    .result(this.productRepository.findAll())
+                    .build();
+     }
+
+     public ApiResponse<Product> updateProduct(Long id, Product product) {
+          Optional<Product> optionalProd = this.findById(id);
+          if (optionalProd.isEmpty()) {
+               throw new AppException(ErrorCode.PRODUCT_NOT_EXISTED);
+          }
+          Product dbProduct = optionalProd.get();
+          dbProduct.setName(product.getName());
+          dbProduct.setDescription(product.getDescription());
+          dbProduct.setPrice(product.getPrice());
+          dbProduct.setStockQuantity(product.getStockQuantity());
+          dbProduct.setSoldQuantity(product.getSoldQuantity());
+          dbProduct.setCategory(product.getCategory());
+          this.productRepository.save(dbProduct);
+          return ApiResponse.<Product>builder()
+                    .code(1000)
+                    .message("Update product with id " + id + " successfully!")
+                    .result(dbProduct)
+                    .build();
+     }
+
+     public ApiResponse<Void> deleteProductById(Long id) {
+          var prod = this.findById(id);
+          if (prod.isEmpty()) {
+               throw new AppException(ErrorCode.PRODUCT_NOT_EXISTED);
+          }
+          this.productRepository.delete(prod.get());
+          return ApiResponse.<Void>builder()
+                    .code(1000)
+                    .message("Delete product with id " + id + " successfully!")
+                    .result(null)
+                    .build();
      }
 }
